d86914d7ca58a0d5b8802d02758a08997ea3ed3b
pappukrs <digitalpappu101@gmail.com>
2024-12-16 00:46:15 +0530

    dj

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 require("dotenv").config();
 
-const appointmentRoutes = require("./routes/appointmentRoutes");
+// const appointmentRoutes = require("./routes/appointmentRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,20 +14,18 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-})
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose.connect(process.env.MONGO_URI, {
+// })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 
-  app.get('/blog',(req,res)=>{
+  app.get('/',(req,res)=>{
     res.send("hello world")
   })
 
 // Routes
-app.use("/api", appointmentRoutes);
+// app.use("/api", appointmentRoutes);
 
 // Start server
 app.listen(PORT, () => {
